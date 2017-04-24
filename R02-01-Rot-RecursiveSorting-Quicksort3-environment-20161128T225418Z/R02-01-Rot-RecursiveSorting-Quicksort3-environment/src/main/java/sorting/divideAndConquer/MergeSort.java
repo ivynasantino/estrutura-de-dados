c4b6841bf0caa2da44bf539e30468f2c7affb442
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2017-04-24 14:18:12 -0300

    Quick e merge

---
+++ R02-01-Rot-RecursiveSorting-Quicksort3-environment-20161128T225418Z/R02-01-Rot-RecursiveSorting-Quicksort3-environment/src/main/java/sorting/divideAndConquer/MergeSort.java
@@ -0,0 +1,80 @@
+package sorting.divideAndConquer;
+
+
+import sorting.AbstractSorting;
+
+/**
+ * Merge sort is based on the divide-and-conquer paradigm. The algorithm
+ * consists of recursively dividing the unsorted list in the middle, sorting
+ * each sublist, and then merging them into one single sorted list. Notice that
+ * if the list has length == 1, it is already sorted.
+ */
+public class MergeSort<T extends Comparable<T>> extends AbstractSorting<T> {
+
+	@Override
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+		if (validaParamentros(array, leftIndex, rightIndex))
+			return;
+		
+		if (leftIndex < rightIndex) {
+			int middle = leftIndex + (rightIndex - leftIndex) / 2;
+			sort(array, leftIndex, middle);
+			sort(array, middle + 1, rightIndex);
+			merge(array, leftIndex, middle, rightIndex);
+		}
+		
+	}
+	
+	private void merge(T[] array, int leftIndex, int middle, int rightIndex){
+		int size = array.length;
+		T[] aux = (T[]) new Comparable[size];
+		
+		for (int i = leftIndex; i <= rightIndex; i++) {
+			aux[i] = array[i];
+		}
+		
+		int i = leftIndex;
+		int k = leftIndex;
+		int mid = middle + 1;
+		
+		while (i <= middle && mid <= rightIndex) {
+			if (aux[i].compareTo(aux[mid]) < 0) {
+				array[k] = aux[i];
+				i++;
+			}
+			
+			else {
+				array[k] = aux[mid];
+				mid++;
+			}
+			
+			k++;
+		}
+		
+		while (i <= middle) {
+			array[k] = aux[i];
+			k++;
+			i++;
+		}
+		
+		while (mid <= middle) {
+			array[k] = aux[mid];
+			k++;
+			mid++;
+		}
+	}
+	
+	private boolean validaParamentros(T[] array, int leftIndex, int rightIndex) {
+		if (array == null || array.length <= 1) 
+			return false;
+		
+		
+		if (leftIndex <= rightIndex || leftIndex < 0 || rightIndex <= 0) 
+			return false;
+		
+		if (leftIndex >= array.length || rightIndex > array.length) 
+			return false;
+		
+		return true;
+	}
+}
---
+++ R02-01-Rot-RecursiveSorting-Quicksort3-environment-20161128T225418Z/R02-01-Rot-RecursiveSorting-Quicksort3-environment/src/main/java/sorting/divideAndConquer/QuickSort.java
@@ -0,0 +1,60 @@
+package sorting.divideAndConquer;
+
+import sorting.AbstractSorting;
+import util.Util;
+
+/**
+ * Quicksort is based on the divide-and-conquer paradigm. The algorithm chooses
+ * a pivot element and rearranges the elements of the interval in such a way
+ * that all elements lesser than the pivot go to the left part of the array and
+ * all elements greater than the pivot, go to the right part of the array. Then
+ * it recursively sorts the left and the right parts. Notice that if the list
+ * has length == 1, it is already sorted.
+ */
+public class QuickSort<T extends Comparable<T>> extends AbstractSorting<T> {
+
+	@Override
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+		if (validaParamentros(array, leftIndex, rightIndex)) 
+			return;
+		
+		if (leftIndex < rightIndex) {
+			int pivot = particion(array, leftIndex, rightIndex);
+			
+			sort(array, leftIndex, pivot - 1);
+			sort(array, pivot + 1, rightIndex);
+		}
+	}
+	
+	private int particion(T[] array, int leftIndex, int rightIndex) {
+		
+		T pivot = array[leftIndex];
+		int inicial = leftIndex;
+		  
+		for (int j = leftIndex + 1; j <= rightIndex; j++) {
+			if (array[j].compareTo(pivot) < 0) {
+				inicial++;
+				Util.swap(array, inicial, j);
+				}
+			}
+		  
+		Util.swap(array, leftIndex, inicial);
+		  
+		return inicial;
+		
+	}
+	
+	private boolean validaParamentros(T[] array, int leftIndex, int rightIndex) {
+		if (array == null || array.length <= 1) 
+			return false;
+		
+		
+		if (leftIndex <= rightIndex || leftIndex < 0 || rightIndex <= 0) 
+			return false;
+		
+		if (leftIndex >= array.length || rightIndex > array.length) 
+			return false;
+		
+		return true;
+	}
+}
